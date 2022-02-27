ab8e3d04711f0e92adcf593ea9fc26e9fa7d6912
Ayush Singh <theayushsingh72@gmail.com>
2022-02-27 23:12:27 +0530

    first

---
+++ HandTrackingMin.py
@@ -0,0 +1,47 @@
+import cv2
+import mediapipe as mp
+import time
+
+cap = cv2.VideoCapture(0)
+
+mpHand=mp.solutions.hands
+
+#static image_mode false
+hands=mpHand.Hands(False,2)
+mpDraw=mp.solutions.drawing_utils
+
+
+#for fps
+pTime=0
+cTime=0
+
+while True:
+    success,img=cap.read()
+    
+    #simple converts color 
+    imgRGB=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+
+    results=hands.process(imgRGB)
+    print(results)
+
+
+    #results.multi_hand_landmarks     shows hand landmarkers 21 of them to track hand locations
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            for id,lm in enumerate(handLms.landmark):
+                print(id,lm)
+                # gives x y z values of markers
+            
+            #draw landmarks over images 
+            #mpDraw.draw_landmarks(img,handLms)
+            mpDraw.draw_landmarks(img,handLms,mpHand.HAND_CONNECTIONS)
+    
+    
+    cTime=time.time()
+    fps=1/(cTime-pTime)
+    pTime=cTime
+    cv2.putText(img,str(int(fps)),(10,30),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255),2)
+    
+    
+    cv2.imshow("Image",img)
+    cv2.waitKey(1)
